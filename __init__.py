8b4571fa88e7c224134398a4d17be4dca12a3447
electronic Max <electronic@gmail.com>
2013-03-30 14:55:58 -0700

    progress

---
+++ __init__.py
@@ -0,0 +1,1 @@
+## declare views here
---
+++ html/js/cdm.js
@@ -0,0 +1,7 @@
+// base cdm app
+
+var app = angular.module('cdm', ['ngResource', 'ui'])
+	.config(function($routeProvider) {
+		$routeProvider.when();
+	});
+
