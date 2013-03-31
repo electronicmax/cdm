cf21b6ba78c8db2ee7c39955d447564e637394e2
electronic max <electronic@gmail.com>
2013-03-31 04:55:06 +0100

    happy times and good fellows

--- html/js/cdm.js
+++ html/js/cdm.js
@@ -1,7 +1,29 @@
-// base cdm app
+/*global $,_,document,window,console,escape,Backbone,exports,WebSocket */
+/*jslint vars:true todo:true sloppy:true */
 
-var app = angular.module('cdm', ['ngResource', 'ui'])
+// base cdm app -- including base routes
+
+var app = angular
+	.module('cdm', ['ui'])
 	.config(function($routeProvider) {
-		$routeProvider.when();
+		$routeProvider
+			.when('/people', { templateUrl: 'partials/people.html', controller:'PeopleController'})
+			.when('/people/:personId', { templateUrl: 'partials/person.html', controller:'PersonController'})
+			.when('/people/:personId/add/property', { templateUrl: 'partials/add_property.html', controller:'AddProperty'})
+			.when('/people/:personId/add/measurement', { templateUrl: 'partials/add_measurement.html', controller:'AddMeasurement'})
+			.when('/people/:personId/:viewID', { templateUrl: 'partials/view.html', controller:'ViewController'})
+			.when('/login', { templateUrl: 'partials/login.html', controller:'LoginController'})
+			.otherwise({redirectTo:'/people'});
+	})
+	.factory('webbox',function() {
+		var exports = {};
+		var d = exports.loaded = new $.Deferred();
+		WebBox.load().then(function() {
+			exports.u = window.u = WebBox.utils;
+			exports.store = window.store = new WebBox.Store();
+			d.resolve(exports);
+		});
+		return exports;
 	});
+
 
