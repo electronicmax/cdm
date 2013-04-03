8fca06508421796d0228f472fd91975526d30aff
electronic max <electronic@gmail.com>
2013-04-03 01:47:39 +0100

    moving things out of controllers

---
+++ html/js/controllers/login.js
@@ -0,0 +1,31 @@
+
+function LoginController($scope, $location, webbox) {
+	var store;
+	safe_apply($scope, function() { $scope.loading = 1; });	
+	$scope.login = function(username,password) {
+		if (store !== undefined) {
+			store.login(username,password).then(function() {
+				safe_apply($scope, function() { $location.path('/people'); });				
+			}).fail(function(e) {
+				safe_apply($scope, function() {
+					// var msg = 'Sorry, could not ' + e.statusText;
+					var msg = "Incorrect username or password - <br>Please try again.";
+					u.log('setting error to be ', e.statusText);
+					$scope.error = msg;
+				});				
+			});
+			return;
+		}
+		throw new RuntimeError('webbox not initialising properly');
+	};
+	webbox.loaded.then(function(webbox) {
+		console.log('webbox loaded then ');
+		var u = $scope.u = webbox.u;
+		window.webbox = webbox;
+		store = window.store = webbox.store;		
+		store.toolbar.setVisible(false);
+		safe_apply($scope, function() { $scope.loading = 0; });
+		store.logout();
+		console.log('logout ');
+	});	
+}
---
+++ html/js/controllers/person.js
@@ -0,0 +1,4 @@
+
+function PersonController($scope, webbox, $routeParams){
+	console.log('rout params ', $routeParams);
+}
